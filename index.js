c5a18145e7f6aca9fb88151dfa6ef220a7dc07fb
Ayotayo <ayotayobadejo@gmail.com>
2023-04-18 13:03:26 -1100

    try to solve the hosting

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/home', (req, res)=>{
         })
 })
 
-const port = process.env.PORT || 9000
+const port = process.env.PORT || 4000
 
 app.listen(port, ()=>{
     console.log(`listening on: https://localhost:${port}`);
